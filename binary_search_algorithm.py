7dd1208b7015199c841e468230c826513f85408b
kaiserkey <kaiserkey2@gmail.com>
2023-03-05 16:36:26 -0300

    busqueda binaria en python

--- binary_search_algorithm.py
+++ binary_search_algorithm.py
@@ -3,16 +3,16 @@
 """
 
 def binarySearch(list, item):
-    #inicializamos las variables min y max para almacenar el indice minimo y maximo de la lista
-    min = 0
-    max = len(list) - 1
+    #inicializamos las variables left y right para almacenar el indice izquierdo y derecho de la lista
+    left = 0
+    right = len(list) - 1
     #creamos un contador para contar el numero de iteraciones
     count = 0
     
-    #iteramos mientras el indice minimo sea menor o igual al indice maximo
-    while min <= max:
+    #iteramos mientras el indice izquierdo sea menor o igual al indice derecho
+    while left <= right:
         #calculamos el indice medio de la lista
-        mid = (min + max) // 2
+        mid = (left + right) // 2
         #obtenemos el valor del elemento en el indice medio
         guess = list[mid]
         #aumentamos el contador
@@ -24,25 +24,25 @@ def binarySearch(list, item):
             return mid
         #si el valor del elemento en el indice medio es mayor al valor buscado
         elif guess > item:
-            max = mid - 1
+            right = mid - 1
         #si el valor del elemento en el indice medio es menor al valor buscado
         else:
-            min = mid + 1
+            left = mid + 1
     #si no se encuentra el elemento regresamos -1
     return -1
 
 # lista de numeros primos
-listaNumerosPrimos = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+listOfPrime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 #imprimimos la lista de numeros primos
 print("Para la siguiente lista de nuemros primos:")
-for n in range(0, len(listaNumerosPrimos)):
+for n in range(0, len(listOfPrime)):
     print("------", end="")    
 print()
-for n in range(0, len(listaNumerosPrimos)):
-    print("|",listaNumerosPrimos[n],"|", end="")
+for n in range(0, len(listOfPrime)):
+    print("|",listOfPrime[n],"|", end="")
 print()
-for n in range(0, len(listaNumerosPrimos)):
+for n in range(0, len(listOfPrime)):
     print("------", end="")  
 print()
 
@@ -50,11 +50,11 @@ print()
 n = int(input("Ingrese el numero que desea buscar: "))
 
 #obtenemos el indice del elemento buscado
-result = binarySearch(listaNumerosPrimos, n)
+result = binarySearch(listOfPrime, n)
 
 #imprimimos el resultado
 if result != -1:
     print(f"El numero {n} se encuentra en la lista en el indice {result}")
-    print(f"El contenido de la lista en el indice {result} es {listaNumerosPrimos[result]} ")
+    print(f"El contenido de la lista en el indice {result} es {listOfPrime[result]} ")
 else: 
     print(f"El numero {n} no se encuentra en la lista")
